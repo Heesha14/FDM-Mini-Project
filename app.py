f592247b8bfae56a6631c576a4a1d469a0c34fb3
Udayantha03 <it19167138@my.sliit.lk>
2021-10-03 21:27:38 +0530

    final_classification_status

--- app.py
+++ app.py
@@ -75,11 +75,11 @@ def predict():
             output = 'No Loan'
 
         if prediction == 0 or prediction == 1:
-            result = 'Customer Is Not Eligible'
+            result = 'Customer Is Not Eligible For Credit Card.'
         elif prediction == 2 or prediction == 3:
-            result = 'Customer Is Eligible'
+            result = 'Customer Is Eligible For Credit Card'
 
-    return render_template('classification.html', prediction_text='Status : {}'.format(output), prediction_output= 'Result : {}'.format(result))
+    return render_template('classification.html', prediction_text='Status : {}'.format(output), prediction_output= 'System Recommendation : {}'.format(result))
 
 
 @app.route("/rule", methods=["GET", "POST"])
