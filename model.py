76c114ef5e81141863a951293d5fe01795fe694a
Ri <szri8802@gmail.com>
2021-10-03 13:43:16 +0530

    bad

--- model.py
+++ model.py
@@ -11,8 +11,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 def predict_classification(gender, car, realty, child, income, income_type, educ_type, fam_stat, hou_type, age, exp, occ, fam_mem, month):
+
     dataset = pd.read_csv('Credit_Card_final.csv')
 
+    # print(dataset.shape)
     dataset = dataset.iloc[:, 1:16]
     # print(dataset.head())
     # Your code goes here
@@ -20,21 +22,19 @@ def predict_classification(gender, car, realty, child, income, income_type, educ
     y = dataset['STATUS'].values # Target vector
     # print('X shape: {}'.format(np.shape(X)))
     # print('y shape: {}'.format(np.shape(y)))
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
-    rf = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
-    rf.fit(X_train, y_train)
-    prediction_test = rf.predict(X=X_test)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    random_model = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+    random_model.fit(X_train, y_train)
+    prediction_test = random_model.predict(X=X_test)
 
     # Accuracy on Test
-    print("Training Accuracy is: ", rf.score(X_train, y_train))
+    print("Training Accuracy is: ", random_model.score(x_train, y_train))
     # Accuracy on Train
-    print("Testing Accuracy is: ", rf.score(X_test, y_test))
+    print("Testing Accuracy is: ", random_model.score(x_test, y_test))
 
-    # print(rf.predict([[1, 1, 1, 0, 112500, 0, 0, 0, 0, 58.83, 3.106, 16, 2, 1, 1, 1]]))
+    # print(rf.predict([[1,1,1,0,112500,4,4,1,1,59,3,16,2,30]]))
 
-    pickle.dump(rf, open('model.pkl', 'wb'))
+    pickle.dump(random_model, open('model.pkl', 'wb'))
 
     model = pickle.load(open('model.pkl', 'rb'))
     output = model.predict([[gender, car, realty, child, income, income_type, educ_type, fam_stat, hou_type, age, exp, occ, fam_mem, month]])
